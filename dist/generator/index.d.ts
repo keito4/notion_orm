fcad40907e46420f8446a73882f6523b87b709dc
keito4 <newton30000@gmail.com>
2025-01-10 18:57:17 +0900

    delete dist folder

--- dist/generator/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './typeGenerator';
-export * from './clientGenerator';
--- dist/utils/logger.d.ts
+++
@@ -1,9 +0,0 @@
-type LogArgs = unknown[];
-export declare const logger: {
-    debug: (message: string, ...args: LogArgs) => void;
-    success: (message: string, ...args: LogArgs) => void;
-    error: (message: string, error?: unknown) => void;
-    info: (message: string, ...args: LogArgs) => void;
-    warn: (message: string, ...args: LogArgs) => void;
-};
-export {};
